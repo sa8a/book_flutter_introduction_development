8b32ab095347c3223d8f87b067663afb06b8a89c
sa8a <jpkrllll@yahoo.co.jp>
2023-02-06 00:40:41 +0900

    P49: 機能の実装

--- lib/main.dart
+++ lib/main.dart
@@ -26,14 +26,41 @@ class _GridViewPageState extends State<GridViewPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      floatingActionButton: FloatingActionButton(onPressed: () {}),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(
+              Icons.grid_on,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(
+              Icons.grid_off,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(
+              Icons.arrow_upward,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
         ),
-        itemCount: 2,
+        itemCount: 20,
         itemBuilder: (BuildContext context, int index) {
           return Container(
             color: Colors.black,
