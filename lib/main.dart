8f05f3328e8d2ed5830c8c28d943d4f44acadb0f
sa8a <jpkrllll@yahoo.co.jp>
2023-02-09 09:22:29 +0900

    P64: 基本的なUIの実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'widgets/page_gridview.dart';
+import 'widgets/page_sliver_appbar.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Material App',
-      home: GridViewPage(), // P45 GridView
+      // home: GridViewPage(), // P45
+      home: SliverAppBarPage(), // P63
     );
   }
 }
---
+++ lib/widgets/page_sliver_appbar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class SliverAppBarPage extends StatefulWidget {
+  const SliverAppBarPage({super.key});
+
+  @override
+  State<SliverAppBarPage> createState() => _SliverAppBarPageState();
+}
+
+class _SliverAppBarPageState extends State<SliverAppBarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.blueAccent.withOpacity(0.3),
+            floating: true,
+            pinned: true,
+            snap: false,
+            expandedHeight: 180,
+            toolbarHeight: 60,
+            flexibleSpace: FlexibleSpaceBar(
+              title: const Text(
+                '記事カテゴリー',
+              ),
+              background: Container(
+                width: double.infinity,
+                child: Image.network(
+                  'https://images.pexels.com/photos/267392/pexels-photo-267392.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
