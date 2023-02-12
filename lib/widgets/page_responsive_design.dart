05bc7b34047f5b25fed0c5c46ca9b2a5de38b05f
sa8a <jpkrllll@yahoo.co.jp>
2023-02-12 21:48:56 +0900

    P91: 静的UIの実装

---
+++ lib/widgets/page_responsive_design.dart
@@ -0,0 +1,93 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class ResponsiveDesignPage extends StatelessWidget {
+  const ResponsiveDesignPage({
+    Key? key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade600,
+      appBar: AppBar(
+        title: const Text('様々なデバイスに対応する'),
+      ),
+      body: const _Body(),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          const SizedBox(height: 60),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              "Flutter is Google's UI toolkit for building beautiful, natively compiled",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                height: 1.85,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(height: 60),
+          const FlutterLogo(size: 240),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              'Fast Development Paint your app to life in milliseconds with Stateful Hot Reload. Use a rich set of ',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                height: 1.85,
+              ),
+            ),
+          ),
+          const SizedBox(height: 60),
+          SizedBox(
+            width: 300,
+            height: 42,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(
+                  color: Colors.white,
+                ),
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              ),
+              onPressed: () {},
+              child: const Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: Text(
+                  'get started',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
