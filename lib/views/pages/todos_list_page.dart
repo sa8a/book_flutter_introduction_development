72777f9ced1fda4921128ef29d658f6855f954b1
sa8a <jpkrllll@yahoo.co.jp>
2023-02-19 00:23:02 +0900

    P160: ToDoリスト画面のファイル作成

---
+++ lib/views/pages/todos_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodosListPage extends StatefulWidget {
+  const TodosListPage({super.key});
+
+  @override
+  State<TodosListPage> createState() => _TodosListPageState();
+}
+
+class _TodosListPageState extends State<TodosListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
